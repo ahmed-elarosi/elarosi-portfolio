ce8b061bc8e046eaec8a17de1eccb9df8d6385e0
Ahmed Elarosi <cabry95@protonmail.com>
2025-11-15 08:45:49 +0100

    add useTheme func

---
+++ src/context/ThemeContext.jsx
@@ -0,0 +1,11 @@
+import React, { createContext, useContext, useState, useEffect } from "react";
+
+const ThemeContext = createContext();
+
+export const useTheme = () => {
+    const context = useContext(ThemeContext);
+    if (!context) {
+        throw new Error("useTheme must be used within ThemeProvider");
+    }
+    return context;
+};
