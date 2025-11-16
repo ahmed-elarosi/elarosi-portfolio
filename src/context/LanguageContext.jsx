375c3396c45d13b29dbd5fe3a94612a6483dfe04
Ahmed Elarosi <cabry95@protonmail.com>
2025-11-16 11:23:06 +0100

    add languageContext

--- src/context/LanguageContext.jsx
+++ src/context/LanguageContext.jsx
@@ -3,3 +3,11 @@ import enTranslations from "../translations/en.json";
 import deTranslations from "../translations/de.json";
 
 const languageContext = createContext();
+
+export const useLanguage = () => {
+    const context = useContext(languageContext);
+    if (!context) {
+        throw new Error("useLanguage must be used within LanguageProvider");
+    }
+    return context;
+};
