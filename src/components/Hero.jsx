0c9279a6bf6efd1b2a1399f9d72a2a3d1f9369dc
Ahmed Elarosi <cabry95@protonmail.com>
2025-11-18 20:08:16 +0100

    start Hero component func

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,3 +2,8 @@ import React from "react";
 import { Download } from "lucide-react";
 import { useTheme } from "../context/ThemeContext";
 import { useLanguage } from "../context/LanguageContext";
+
+const Hero = () => {
+    const { isDark } = useTheme();
+    const { lang, t } = useLanguage();
+};
