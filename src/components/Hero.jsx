d51f5e765447e8d948c5e6df0ebb76b22c783c67
Ahmed Elarosi <cabry95@protonmail.com>
2025-11-20 20:23:34 +0100

    orgniz my file

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,14 +4,14 @@ import { useTheme } from "../context/ThemeContext";
 import { useLanguage } from "../context/LanguageContext";
 
 const Hero = () => {
-    const { isDark } = useTheme();
-    const { lang, t } = useLanguage();
+  const { isDark } = useTheme();
+  const { lang, t } = useLanguage();
 
-    const downloadCv = () => {
-        const cvUrl = lang === "en" ? "assets/CV_EN.pdf" : "/assets/CV_DE.pdf";
-        const link = document.createElement("a");
-        link.href = cvUrl;
-        link.download = `Ahmed_Elarosi_CV_${lang.toUpperCase()}.pdf`;
-        link.click();
-    };
+  const downloadCv = () => {
+    const cvUrl = lang === "en" ? "assets/CV_EN.pdf" : "/assets/CV_DE.pdf";
+    const link = document.createElement("a");
+    link.href = cvUrl;
+    link.download = `Ahmed_Elarosi_CV_${lang.toUpperCase()}.pdf`;
+    link.click();
+  };
 };
