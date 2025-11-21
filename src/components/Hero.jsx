cfece1de45b6ce29e53b786d8298fdc81c3915b7
Ahmed Elarosi <cabry95@protonmail.com>
2025-11-21 22:09:34 +0100

    return section hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,14 +4,20 @@ import { useTheme } from "../context/ThemeContext";
 import { useLanguage } from "../context/LanguageContext";
 
 const Hero = () => {
-  const { isDark } = useTheme();
-  const { lang, t } = useLanguage();
+    const { isDark } = useTheme();
+    const { lang, t } = useLanguage();
 
-  const downloadCv = () => {
-    const cvUrl = lang === "en" ? "assets/CV_EN.pdf" : "/assets/CV_DE.pdf";
-    const link = document.createElement("a");
-    link.href = cvUrl;
-    link.download = `Ahmed_Elarosi_CV_${lang.toUpperCase()}.pdf`;
-    link.click();
-  };
+    const downloadCv = () => {
+        const cvUrl = lang === "en" ? "assets/CV_EN.pdf" : "/assets/CV_DE.pdf";
+        const link = document.createElement("a");
+        link.href = cvUrl;
+        link.download = `Ahmed_Elarosi_CV_${lang.toUpperCase()}.pdf`;
+        link.click();
+    };
+    return (
+        <section
+            id="home"
+            className={`min-h-screen flex items-center pt-20 ${isDark ? "bg-gradient-to-br from-gray-900 via-purple-900 to-blue-900" : "bg-gradient-to-br from-blue-50 via-purple-50 to-pink-50"}`}
+        ></section>
+    );
 };
